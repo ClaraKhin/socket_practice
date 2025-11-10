8ae8a089c044a452837f46c0abd82a86890d6ba7
Khin <khin.thiri.kyaw@team.studioamk.com>
2025-11-10 17:30:20 +0630

    updated frontend and server

---
+++ client/src/component/Input.jsx
@@ -0,0 +1,10 @@
+const Input = ({ placeholder }) => {
+  return (
+    <div>
+      <div>
+        <input type="text" className="input-field" placeholder={placeholder} />
+      </div>
+    </div>
+  );
+};
+export default Input;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const { createServer } = require('http');
 const { Server } = require('socket.io');
 
 const httpServer = createServer();
-const socket = new Server(httpServer, { cors: { origin: '*' } });
+const socket = new Server(httpServer, { cors: { origin: "*" } });
 
 socket.on('connection', (socket) => {
 
